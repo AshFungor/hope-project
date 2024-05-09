e466f145b67ad7e2b8e4d602cacd6a725dd41f66
sa.smirnov <ser1a@ya.ru>
2024-05-09 21:50:39 +0300

    лк

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ from flask import Flask
 from .config import Config
 
 from .routes.main import main
-
+from .routes.person_lk import person_lk
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     app.register_blueprint(main)
+    app.register_blueprint(person_lk)
 
     return app
---
+++ app/routes/person_lk.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template, url_for
+
+person_lk = Blueprint('person_lk', __name__)
+
+
+@person_lk.route('/person_lk')
+def person_cabinet():
+    return "Кабинет!!!"
