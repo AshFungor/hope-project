1455c6e76137e582a1412fb219184940308d99b1
lrq <miv1kk.1972@gmail.com>
2024-05-12 02:32:13 +0300

    templ

--- app/routes/person_lk.py
+++ app/routes/person_lk.py
@@ -6,4 +6,4 @@ person_lk = Blueprint('person_lk', __name__)
 @person_lk.route('/person_lk')
 def person_cabinet():
     """Личный кабинет пользователя"""
-    return "Кабинет!!!"
+    return render_template('main/person_lk_page.html')
