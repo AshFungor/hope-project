1b259167b8be0b6502ded2a098ed62ad5f560956
MachoGamesStudio <110656350+MachoGamesStudio@users.noreply.github.com>
2024-05-12 11:38:11 +0300

    romachizh

--- scripts/create_db_script.py
+++ scripts/create_db_script.py
@@ -17,7 +17,7 @@ cursor.execute("""
                     birthday DATE, 
                     bank_account_id INTEGER
                     ),
-                    
+
                 CREATE TABLE IF NOT EXIST Goals
                     (
                     id INTEGER PRIMARY KEY,
@@ -26,19 +26,19 @@ cursor.execute("""
                     bank_account_id INTEGER FOREIGN KEY,
                     complete BOOL DEFAULT FALSE
                     ),
-                    
+
                 CREATE TABLE IF NOT EXIST BankAccounts
                     (
                     id INTEGER UNIQUE PRIMARY KEY, 
                     balance INTEGER
                     ),
-                    
+
                 CREATE TABLE IF NOT EXIST Companies
                     (
                     id INTEGER PRIMARY KEY,
                     bank_account_id INTEGER FOREIGN KEY
                     ),
-                
+
                 CREATE TABLE IF NOT EXIST Deal
                     (
                     id INTEGER PRIMARY KEY,
@@ -49,22 +49,20 @@ cursor.execute("""
                     amount INTEGER,
                     status INTEGER,
                     ),
-                    
+
                 CREATE TABLE IF NOT EXIST Products2BankAccounts
                     (
                     id INTEGER PRIMARY KEY,
-                    bank_account_id INTEGER FOREIGN KEY
-                    product_id INTEGER FOREIGN KEY
+                    bank_account_id INTEGER FOREIGN KEY,
+                    product_id INTEGER FOREIGN KEY,
                     count INTEGER
-                    ),
-                
+                    );
+
                 CREATE TABLE IF NOT EXIST Products
                     (
                     id INTEGER PRIMARY KEY,
                     name TEXT
-                    )
-                
-                    
+                    );
                 """)
 
 conn.commit()
