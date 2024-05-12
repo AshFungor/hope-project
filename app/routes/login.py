f1a9c815b4194a9247b5efb9509fda1329e7bb4f
MachoGamesStudio <110656350+MachoGamesStudio@users.noreply.github.com>
2024-05-12 14:03:30 +0300

    romachizh1

--- app/routes/login.py
+++ app/routes/login.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, url_for
 
-company_lk = Blueprint('login', __name__)
+login = Blueprint('login', __name__)
 
 
-@company_lk.route('/login')
+@login.route('/login')
 def login():
     """Страничка входа"""
     return "Логинка!!!"
