70ce4d6bc55fbcdd6035e9abf8989e1c3a555d28
sa.smirnov <ser1a@ya.ru>
2024-05-12 09:19:38 +0300

    бд models

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, url_for
 
+
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 @main.route('/index')
 def index():
+
     return render_template('main/index.html')
--- app/routes/person_lk.py
+++ app/routes/person_lk.py
@@ -1,9 +1,17 @@
 from flask import Blueprint, render_template, url_for
+from ..extensions import db
+from ..models.user import User
 
 person_lk = Blueprint('person_lk', __name__)
 
-
 @person_lk.route('/person_lk')
 def person_cabinet():
+
+    # TODO УДАЛИТЬ ПОСЛЕ НАПИСАНИЯ МОДЕЛЕЙ!
+    # ПРИМЕР СОЗДАНИЯ ОБЪЕКТА В БД
+
+    user = User(name="test")
+    db.session.add(user)
+    db.session.commit()
     """Личный кабинет пользователя"""
-    return "Кабинет!!!"
+    return user.name
