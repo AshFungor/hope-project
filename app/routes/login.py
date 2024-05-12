23470d642306ea77290c1fd03441a87c6dbed282
MachoGamesStudio <110656350+MachoGamesStudio@users.noreply.github.com>
2024-05-12 15:08:19 +0300

    link for btn_login

--- app/routes/login.py
+++ app/routes/login.py
@@ -5,6 +5,6 @@ login = Blueprint('login', __name__)
 
 
 @login.route('/login')
-def login():
+def authorization():
     """Страничка входа"""
     return render_template("login/authorization.html")
