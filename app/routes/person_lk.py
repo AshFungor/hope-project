041d97350304ba9a0c2ec9bd2f21c2f52181dd67
lrq <lrq@noreply.gitverse.ru>
2024-05-15 14:04:03 +0000

    [webapp] some changes to routes and templates Co-authored-by: lrq <lrq@noreply.gitverse.ru> Co-committed-by: lrq <lrq@noreply.gitverse.ru>

--- app/routes/person_lk.py
+++ app/routes/person_lk.py
@@ -3,15 +3,15 @@ from app.env import env
 from app.models.user import User
 
 person_lk = Blueprint('person_lk', __name__)
-
+db = env.db.impl()
 @person_lk.route('/person_lk')
 def person_cabinet():
 
     # TODO УДАЛИТЬ ПОСЛЕ НАПИСАНИЯ МОДЕЛЕЙ!
     # ПРИМЕР СОЗДАНИЯ ОБЪЕКТА В БД
-
+    
     user = User(name="test")
-    env.db.session.add(user)
-    env.db.session.commit()
+    db.session.add(user)
+    db.session.commit()
     """Личный кабинет пользователя"""
     return render_template('main/person_lk_page.html')
