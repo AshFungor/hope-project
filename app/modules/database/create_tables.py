192b2e7d2c295432da7c4ff981ab693b82d549f2
Lea <egorka.witle@gmail.com>
2024-05-22 12:26:18 +0300

    [webapp]
    used a csv parser for adding tables

--- app/modules/database/create_tables.py
+++ app/modules/database/create_tables.py
@@ -1,22 +1,12 @@
-import os, csv
+# required library
+import datetime
+
 from app.env import env
 import app.models as models
-import datetime
 from app.modules.database.validators import CurrentTimezone
 
 
 class TablesCreator:
-
-    def __init__(self):
-        self._created_start_tables_check = False
-
-    @property
-    def created_tables(self):
-        return self._created_start_tables_check
-
-    @created_tables.getter
-    def check(self):
-        return self._created_start_tables_check
 
     def create_bank_account(self):
         bank_account = models.BankAccount()
@@ -24,56 +14,49 @@ class TablesCreator:
         env.db.impl().session.commit()
         return bank_account.id
 
-    def create_prefectures(self):
-        with open(os.path.join(os.getcwd(), 'app/static/csv/Prefectures.csv'), encoding='utf-8-sig') as csv_prefectures:
-            prefectures = csv.DictReader(csv_prefectures, delimiter=';')
-            for prefecture in prefectures:
-                prefecture_model = models.Prefecture()
-                prefecture_model.bank_account_id = self.create_bank_account()
-                prefecture_model.name = prefecture['Название префектуры'].capitalize()
-                env.db.impl().session.add(prefecture_model)
+    def create_prefectures(self, prefectures):
+        for prefecture in prefectures:
+            prefecture_model = models.Prefecture()
+            prefecture_model.bank_account_id = self.create_bank_account()
+            prefecture_model.name = prefecture['Название префектуры'].capitalize()
+            env.db.impl().session.add(prefecture_model)
         env.db.impl().session.commit()
 
-    def create_cities(self):
-        with open(os.path.join(os.getcwd(), 'app/static/csv/Cities.csv'), encoding='utf-8-sig') as csv_cities:
-            cities = csv.DictReader(csv_cities, delimiter=';')
-            for city in cities:
-                city_model = models.City()
-                city_model.name = city['Название города'].capitalize()
-                query = env.db.impl().session.query(models.Prefecture).filter(
-                    models.Prefecture.name == city['Префектура города'].capitalize()).first()
-                city_model.prefecture_id = query.id
-                city_model.bank_account_id = self.create_bank_account()
-                city_model.location = int(city['Комната'])
-                env.db.impl().session.add(city_model)
+    def create_cities(self, cities):
+        for city in cities:
+            city_model = models.City()
+            city_model.name = city['Название города'].capitalize()
+            query = env.db.impl().session.query(models.Prefecture).filter(
+                models.Prefecture.name == city['Префектура города'].capitalize()).first()
+            city_model.prefecture_id = query.id
+            city_model.bank_account_id = self.create_bank_account()
+            city_model.location = int(city['Комната'])
+            env.db.impl().session.add(city_model)
         env.db.impl().session.commit()
 
-    def create_users(self):
-        with open(os.path.join(os.getcwd(), 'app/static/csv/Users.csv'), encoding='utf-8-sig') as csv_users:
-            users = csv.DictReader(csv_users, delimiter=';')
-            for user in users:
-                list_date_birthday = list(map(int, user['Дата рождения'].split('.')))
-                birthday = datetime.datetime(
-                    year=list_date_birthday[-1],
-                    month=list_date_birthday[-2],
-                    day=list_date_birthday[-3],
-                    tzinfo=CurrentTimezone
-                )
-                user_model = models.User(
-                    self.create_bank_account(),
-                    env.db.impl().session.query(models.City).filter(
-                        models.City.name == user['Город']).first().id,
-                    user['Имя'].capitalize(),
-                    user['Фамилия'].capitalize(),
-                    user['Логин'],
-                    user['Пароль'],
-                    'male' if user['Пол'].lower() == 'мужской' else 'female',
-                    0,
-                    birthday
-                )
-                env.db.impl().session.add(user_model)
-                self.create_start_kit(user_model.bank_account_id)
-
+    def create_users(self, users):
+        for user in users:
+            list_date_birthday = list(map(int, user['Дата рождения'].split('.')))
+            birthday = datetime.datetime(
+                year=list_date_birthday[-1],
+                month=list_date_birthday[-2],
+                day=list_date_birthday[-3],
+                tzinfo=CurrentTimezone
+            )
+            user_model = models.User(
+                self.create_bank_account(),
+                env.db.impl().session.query(models.City).filter(
+                    models.City.name == user['Город']).first().id,
+                user['Имя'].capitalize(),
+                user['Фамилия'].capitalize(),
+                user['Логин'],
+                user['Пароль'],
+                'male' if user['Пол'].lower() == 'мужской' else 'female',
+                0,
+                birthday
+            )
+            env.db.impl().session.add(user_model)
+            self.create_start_kit(user_model.bank_account_id)
         env.db.impl().session.commit()
 
     def create_start_kit(self, bank_account_id: int):
@@ -159,13 +142,11 @@ class TablesCreator:
         env.db.impl().session.commit()
         return product.id
 
-    def create_all_start_tables(self):
-        if not self._created_start_tables_check:
-            self.create_prefectures()
-            self.create_cities()
-            self.create_start_products()
-            self.create_users()
-            self._created_start_tables_check = True
+    def create_all_start_tables(self, prefectures, cities, users):
+        self.create_prefectures(prefectures)
+        self.create_cities(cities)
+        self.create_start_products()
+        self.create_users(users)
 
 
 TbCreator = TablesCreator()
--- app/routes/person_lk.py
+++ app/routes/person_lk.py
@@ -17,50 +17,6 @@ person_lk = Blueprint('person_lk', __name__)
 
 @person_lk.route('/person_lk')
 def person_cabinet():
-
-    # users_id: 5, 6
-    if not TbCreator.check:
-        TbCreator.create_all_start_tables()
-        product_id = TbCreator.create_product('электроника', 'playstation 5', 2)
-        TbCreator.create_products_to_bank_account(
-            bank_account_id=env.db.impl().session.query(models.User).get(1).bank_account_id,
-            product_id=product_id,
-            count=1
-        )
-
-    # accounts = []
-    # # create prefecture
-    # prefecture = models.Prefecture()
-    # account = models.BankAccount()
-    # account.id = 0
-    # accounts.append(account)
-    # prefecture.bank_account_id = accounts[-1].id
-    # # create city
-    # city = models.City()w
-    # city.bank_account_id = accounts[-1].id
-    # city.location = 'city Kek'
-    # city.name = 'Lol and kek'
-    # city.prefecture_id = prefecture.id
-    # # finally, user...
-    # time = datetime.datetime.now(CurrentTimezone)
-    # user = models.User(
-    #     accounts[-1].id,
-    #     city.id,
-    #     'Кек',
-    #     'Лол',
-    #     'kek_login',
-    #     '##yo__^_^__cool',
-    #     'other',
-    #     0,
-    #     time
-    # )
-    #
-    # for account in accounts:
-    #     env.db.impl().session.add(account)
-    # env.db.impl().session.add(prefecture)
-    # env.db.impl().session.add(city)
-    # env.db.impl().session.add(user)
-    # env.db.impl().session.commit()
     """Личный кабинет пользователя"""
     return render_template('main/person_lk_page.html')
 
