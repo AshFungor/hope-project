8ad41141a29a0eab18ff037c2539d421fca04dcc
Lea <egorka.witle@gmail.com>
2024-05-22 12:35:56 +0300

    [webapp] fixed

--- app/routes/upload_csv.py
+++ app/routes/upload_csv.py
@@ -18,11 +18,12 @@ def add_csv():
         csv_users = request.files['users']
         table_users = parse_csv(csv_users)
 
-        TbCreator.create_all_start_tables(
-            prefectures=table_prefectures,
-            cities=table_cities,
-            users=table_users
-        )
+        if csv_prefectures and csv_cities and csv_users:
+            TbCreator.create_all_start_tables(
+                prefectures=table_prefectures,
+                cities=table_cities,
+                users=table_users
+            )
         return redirect(url_for('person_lk.person_cabinet'))
     """страничка для загрузки csv"""
     return render_template('main/upload_csv.html')
