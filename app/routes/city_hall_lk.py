d1e578e59873e4b894a600d5be1f917487ef386b
Lea <egorka.witle@gmail.com>
2024-06-04 22:52:43 +0300

    [webapp] add transaction logic

--- app/routes/city_hall_lk.py
+++ app/routes/city_hall_lk.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, url_for, session
+from flask_login import login_required
 
 city_hall_lk = Blueprint('city_hall_lk', __name__)
 
 
 @city_hall_lk.route('/city_hall_lk')
+@login_required
 def city_hall_cabinet():
     """Личный кабинет мэрии"""
     return render_template('main/city_hall.html', session=session)
