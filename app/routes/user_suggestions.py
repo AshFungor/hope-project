5992312cd289417f5f3e0c29b9463d3dae595d3b
Lea <egorka.witle@gmail.com>
2024-06-04 22:56:36 +0300

    [webapp] suggestions viewing

---
+++ app/routes/user_suggestions.py
@@ -0,0 +1,50 @@
+import logging
+
+import flask
+from flask import Blueprint, render_template, url_for, request
+from flask_login import login_required, current_user
+
+from sqlalchemy import and_
+
+import app.models as models
+from app.modules.database.static import StaticTablesHandler
+from app.env import env
+
+user_suggestions = Blueprint('user_suggestions', __name__)
+
+
+@user_suggestions.route('/user_suggestions', methods=['GET', 'POST'])
+@user_suggestions.route('/user_suggestions/<int:page>', methods=['GET', 'POST'])
+def user_sugs(page=1):
+
+    #  try completed transaction
+    if request.method == 'POST':
+        logging.info(f'user_id: {current_user.id} request for transaction approval')
+        status = 'rejected' if request.form.get('action') == 'Отклонить' else 'approved'
+        result_transaction = StaticTablesHandler.complete_transaction(request.form.get('transaction_id'), status)
+        # result_transaction: (completed: bool, message: str)
+
+        if result_transaction[0]:
+            flask.flash(result_transaction[1])
+            logging.info(f'user_id: {current_user.id} transaction({request.form.get("transaction_id")}) has been completed')
+            env.db.impl().session.commit()
+        else:
+            logging.info('transaction was not completed')
+            flask.flash(result_transaction[1])
+    user_suggs = (
+        env.db.impl().session.query(
+            models.User.name,
+            models.User.bank_account_id,
+            models.Transaction.id,
+            models.Transaction.amount,
+            models.Transaction.count,
+            models.Product.name.label('product_name')
+            )
+            .filter(and_(models.Transaction.status == 'created',
+                         models.Transaction.customer_bank_account_id == current_user.bank_account_id))
+            .join(models.User, models.User.bank_account_id == models.Transaction.seller_bank_account_id)
+            .join(models.Product, models.Product.id == models.Transaction.product_id)
+            .paginate(page=page, per_page=10, error_out=False)
+    )
+    logging.info(f'user_id: {current_user.id} requested your suggestions')
+    return render_template('main/user_suggestions.html', user_suggestions=user_suggs)
