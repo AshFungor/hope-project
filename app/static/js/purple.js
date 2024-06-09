2b8b390ce5cde67d40600ddeb9472c462906b798
miv1kk <96937481+miv1kk@users.noreply.github.com>
2024-06-09 17:31:45 +0300

    [frontend] мне лень придумывать название, просто кнопочки  (#19)

---
+++ app/static/js/purple.js
@@ -0,0 +1,9 @@
+const elsP = document.querySelectorAll('.btn-primary');
+elsP.forEach((el) => {
+    el.classList = 'btn btn-secondary btn-lg';
+});
+
+const elsP2 = document.querySelectorAll('.btn-outline-primary');
+elsP2.forEach((el) => {
+    el.classList = 'btn btn-outline-secondary btn-lg';
+});
