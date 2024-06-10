9e3fbc376c27ca9959c5e4a5a6cec1fffb18ebbc
lrq <miv1kk.1972@gmail.com>
2024-06-10 16:55:02 +0300

    lrq/company-page/pjlsta-fix-the-addresses

--- app/static/js/green.js
+++
@@ -1,9 +0,0 @@
-const elsP = document.querySelectorAll('.btn-primary');
-elsP.forEach((el) => {
-    el.classList = 'btn btn-success btn-lg';
-});
-
-const elsP2 = document.querySelectorAll('.btn-outline-primary');
-elsP2.forEach((el) => {
-    el.classList = 'btn btn-outline-success btn-lg';
-});
