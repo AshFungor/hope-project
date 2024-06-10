cbac72cf73c129a229639ad075b47fead29e6c67
lrq <miv1kk.1972@gmail.com>
2024-06-10 23:15:05 +0300

    ura-city-hall

--- app/routes/city_lk.py
+++ app/routes/city_lk.py
@@ -5,7 +5,7 @@ city_lk = Blueprint('city_lk', __name__)
 
 
 @city_lk.route('/city_lk')
-# @login_required
+@login_required
 def city_cabinet():
     """Личный кабинет города!!"""
     return render_template('main/city_lk_page.html')
