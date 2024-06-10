76c703ad5ec8bbc42c24c1d7cec96f3e655aed51
lrq <miv1kk.1972@gmail.com>
2024-06-10 17:59:42 +0300

    fixed-list-on-company-page/uncommented-login_required

--- app/routes/company_lk.py
+++ app/routes/company_lk.py
@@ -5,7 +5,7 @@ company_lk = Blueprint('company_lk', __name__)
 
 
 @company_lk.route('/company_lk')
-# @login_required
+@login_required
 def company_cabinet():
     """Личный кабинет компании"""
     return render_template('main/company.html')
