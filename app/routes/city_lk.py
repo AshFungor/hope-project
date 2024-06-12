617810ba4c2054a0768850fbeb93ef4a7522e754
Lea <egorka.witle@gmail.com>
2024-06-12 12:23:51 +0300

    [database] fixed fixes

--- app/routes/city_lk.py
+++ app/routes/city_lk.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, url_for
 from flask_login import login_required
 
+from app.env import env
+import app.models as models
+
 city_lk = Blueprint('city_lk', __name__)
 
 
@@ -8,4 +11,9 @@ city_lk = Blueprint('city_lk', __name__)
 @login_required
 def city_cabinet():
     """Личный кабинет города!!"""
+    prefecture_mayor = env.db.impl().session.query(
+        models.City
+    ).join(
+        models.User, models.User.id == models.City.mayor_id
+    )
     return render_template('main/city_lk_page.html')
