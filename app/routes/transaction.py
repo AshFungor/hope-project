5e646ee1c67daa83647592f4ea1597629bcab7f2
miv1kk <96937481+miv1kk@users.noreply.github.com>
2024-06-14 02:26:49 +0300

    [frontend] страницы создания транзакции/входящие транзакции ✔ (#36)

--- app/routes/transaction.py
+++ app/routes/transaction.py
@@ -12,57 +12,17 @@ from app.env import env
 from app.forms.transaction import TransactionForm
 from app.modules.database.static import CurrentTimezone
 
+import app.routes.blueprints as blueprints
 
-# @transaction.route('/make_transaction', methods=['GET', 'POST'])
-# @login_required
-# def transaction_page():
-#     form = TransactionForm(request.form)
-#     if request.method == 'POST':
-#         if form.validate_on_submit():  # validate form
-#             logging.info(f'user({current_user.id}) is trying to create a transaction')
-#             amount = form.amount.data
-#             customer_bank_account_id = form.bank_account.data
+@blueprints.accounts_blueprint.route('/transactiona')
+@login_required
+def transactiona():
+    
+    return render_template('main/transaction.html')
 
-#             # get seller Product2BankAccount and check your count
-#             customer_wallet = env.db.impl().session.query(models.Product2BankAccount).filter(and_(
-#                 models.Product2BankAccount.bank_account_id == customer_bank_account_id,
-#                 models.Product2BankAccount.product_id == 1,
-#             )).first()
-#             if customer_wallet:
-#                 seller_wallet = env.db.impl().session.query(models.Product2BankAccount).filter(and_(
-#                     models.Product2BankAccount.bank_account_id == current_user.bank_account_id,
-#                     models.Product2BankAccount.product_id == 1,
-#                 )).first()
 
-#                 if seller_wallet.count < amount:
-#                     logging.info('transaction was not created')
-#                     flask.flash('У вас недостаточно денег', 'warning')
-#                     return redirect(url_for('transaction.transaction_page'))
-
-#                 # making a transfer
-#                 seller_wallet.count = seller_wallet.count - amount
-#                 customer_wallet.count = customer_wallet.count + amount
-
-#                 # approved transaction
-#                 transaction = models.Transaction(
-#                     product_id=1,
-#                     customer_bank_account_id=customer_bank_account_id,
-#                     seller_bank_account_id=current_user.bank_account_id,
-#                     count=amount,
-#                     amount=amount,
-#                     status='approved',
-#                     updated_at=datetime.datetime.now(tz=CurrentTimezone),
-#                     comment=None,
-#                 )
-#                 env.db.impl().session.add(transaction)
-#                 env.db.impl().session.commit()
-#                 logging.info(f'user({current_user.id}) has created a transaction')
-#                 flask.flash('Перевод успешно выполнен', 'info')
-#             else:
-#                 logging.info('transaction was not created')
-#                 flask.flash('Данного банковского счета не существует', 'warning')
-#         else:
-#             logging.info('transaction was not created')
-#             flask.flash('Вы неверно заполнили форму', 'warning')
-#         return redirect(url_for('transaction.transaction_page'))
-#     return render_template('main/transaction.html', form=form)
+@blueprints.accounts_blueprint.route('/transactiona_view')
+@login_required
+def transactiona_view():
+    
+    return render_template('main/transaction_view.html')
