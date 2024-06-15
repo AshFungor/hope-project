84ee945dfa31410189e4e8bf37d40e69ecadb453
AshFungor <evgenynarkhov2@gmail.com>
2024-06-15 01:40:14 +0000

    lol

--- app/routes/transaction.py
+++ app/routes/transaction.py
@@ -1,19 +1,42 @@
+# base
 import requests
 
+# flask
 import flask
 import flask_login
 
+# local
+from app.env import env
+
+import app.models as models
 import app.routes.blueprints as blueprints
 
 
-blueprints.proposal_blueprint.route('/new_transaction', methods=['GET'])
-flask_login.login_required
-def new_transaction() -> flask.Response:
+@blueprints.proposal_blueprint.route('/view_transactions', methods=['GET'])
+@flask_login.login_required
+def view_transaction():
     # get user bank account id & make payload from it
     user_id = flask_login.current_user.bank_account_id
     payload = {
         'user': user_id
     }
     # make post to local address and get all data for transactions
-    response = requests.post('http://localhost/transaction/view', json=payload).json()
-    return flask.render_template('main/transaction_view.html', transactions=response)
+    response = requests.post('http://nginx/transaction/view', json=payload).json()
+    return flask.render_template('main/view_transaction_view.html', transactions=response)
+
+
+@blueprints.proposal_blueprint.route('/new_transaction', methods=['GET'])
+@flask_login.login_required
+def new_transaction():
+    # get all products
+    products = env.db.impl().session.query(models.Product).all()
+    env.db.impl().session.commit()
+
+    data = []
+    for number, product in zip(range(len(products)), products):
+        data.append(
+            { 'name': product.name, 'number': number }
+        )
+
+    user_id = flask_login.current_user.bank_account_id
+    return flask.render_template('main/make_transaction.html', user_bank_account=user_id, products=data)
