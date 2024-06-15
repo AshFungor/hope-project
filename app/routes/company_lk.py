54dbf3b25d6b40a02e6089d5821dec0a0495add9
Max Lukin <max.lukin@ori.team>
2024-06-16 01:01:26 +0300

    fixing startup

--- app/routes/company_lk.py
+++ app/routes/company_lk.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, render_template, url_for, session
+from flask import Blueprint, render_template, url_for
+from flask_login import login_required
 
 # local
 import app.modules.database.static as static
 import app.routes.blueprints as blueprints
-
-
 @blueprints.accounts_blueprint.route('/company_lk')
 @login_required
 def company_cabinet():
     """Личный кабинет компании"""
-    return render_template('main/company.html', session=session)
+    return render_template('main/company.html')
