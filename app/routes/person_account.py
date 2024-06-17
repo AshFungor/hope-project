0bb4dbacbed5ada4ebdcd3991dae46363ce9560f
KLyustrov <lkostya@mail.ru>
2024-06-17 13:36:39 +0300

    [webapp] Personal cabinet updated Company workers page updated

--- app/routes/person_account.py
+++ app/routes/person_account.py
@@ -1,11 +1,22 @@
 import flask
+from flask import Blueprint, render_template, url_for, request
+from flask_login import login_required, current_user
+
+import sqlalchemy
+
+from app.env import env
+import app.models as models
 
 # local
+import app.modules.database.static as static
 import app.routes.blueprints as blueprints
+from app.models.helpers import get_bank_account_size
 
 
 @blueprints.accounts_blueprint.route('/person_account')
+@login_required
 def person_account():
-    return flask.render_template('main/person_account_page.html')
-
-
+    query_user = sqlalchemy.select(models.User).filter(models.User.id == current_user.id)
+    user = env.db.impl().session.execute(query_user).first()[0]
+    balance = get_bank_account_size(user.bank_account_id)
+    return render_template('main/person_account_page.html', user=user, balance=balance)
