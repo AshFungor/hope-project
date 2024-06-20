79b058f4261f8e1a04c7f9fd6b583cab8b9a23e2
Lea <egorka.witle@gmail.com>
2024-06-20 21:26:56 +0300

    my bad

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,13 +1,15 @@
-from flask import Blueprint, render_template, url_for
-
+from flask import Blueprint, render_template
+from flask_login import LoginManager
 
 # local
 import app.modules.database.static as static
 import app.routes.blueprints as blueprints
 
+login_manager = LoginManager()
+login_manager.login_view = 'login.authorization'
+
 
 @blueprints.main.route('/')
 @blueprints.main.route('/index')
 def index():
-
     return render_template('main/index.html')
