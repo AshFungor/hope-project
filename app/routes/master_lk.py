fa779872ee82a0e2eb085f5640802ffa61a91c5a
Lea <egorka.witle@gmail.com>
2024-06-28 18:52:00 +0300

    merge

--- app/routes/master_lk.py
+++ app/routes/master_lk.py
@@ -2,7 +2,7 @@ import datetime
 import logging
 import flask
 from flask import Blueprint, render_template, url_for
-from flask_login import login_required
+from flask_login import login_required, current_user
 import sqlalchemy
 
 # local
@@ -19,6 +19,7 @@ def master_cabinet():
     """Личный кабинет мастера"""
     return render_template('main/master_lk.html')
 
+
 @blueprints.master_blueprint.route('/edit_data')
 @login_required
 def edit_data():
@@ -26,125 +27,61 @@ def edit_data():
     return render_template('main/master_edit.html')
 
 
-@blueprints.master_blueprint.route('/action_with_products', methods=['POST'])
-@login_required
-def action_with_energy():
-    try:
-        form_id = int(flask.request.form.get('form_id', None))
-    except Exception as Error:
-        logging.warning(f'{Error}')
-        return flask.Response(
-            'Ошибка при добавлении энергии',
-            status=400
-        )
-    if form_id is None:
-        return flask.Response(
-            'Ошибка при добавлении энергии',
-            status=400
-        )
-    count = flask.request.form.get('count', None)
-    bank_account = flask.request.form.get('bank_account', None)
-    if None in (count, bank_account):
-        return flask.Response(
-            'Ошибка получения данных',
-            status=400
-        )
-    product_id = env.db.impl().session.execute(
-        sqlalchemy.select(
-            models.Product.id
-        )
-        .filter_by(name='энергия')
-    ).scalars().first()
-    if product_id is None:
-        return flask.Response(
-            'Энергия не найдена'
-        )
-    check_bank_account = env.db.impl().session.execute(
-        sqlalchemy.select(
-            models.BankAccount
-        )
-        .filter_by(id=bank_account)
-    ).first()
-    if not check_bank_account:
-        return flask.Response(
-            'такого счета не существует',
-            400
-        )
-    products2bank_account = env.db.impl().session.execute(
-        sqlalchemy.select(
-            models.Product2BankAccount
-        )
-        .filter(sqlalchemy.and_(
-            models.Product2BankAccount.product_id == product_id,
-            models.Product2BankAccount.bank_account_id == bank_account
-        ))
-    ).scalars().first()
-    if products2bank_account is None:
-        products2bank_account = models.Product2BankAccount(
-            bank_account_id=int(bank_account),
-            product_id=product_id,
-            count=0
-        )
-        env.db.impl().session.add(products2bank_account)
-        env.db.impl().session.commit()
-    if form_id == 1:
-        products2bank_account.count = products2bank_account.count - int(count)
-    elif form_id == 2:
-        products2bank_account.count += int(count)
-    try:
-        env.db.impl().session.commit()
-    except Exception as Error:
-        flask.Response(
-            f'Произошла ошибка во время выполнения действия',
-            400
-        )
-        logging.warning(f'{Error}')
-    return render_template('main/add_withdrowal.html')
-
-
 @blueprints.master_blueprint.route('/action_with_resource', methods=['POST'])
 @login_required
-def action_with_resource():
+def action_with_product():
+    # check input data
     try:
         form_id = int(flask.request.form.get('form_id', None))
-    except Exception as Error:
-        logging.warning(f'{Error}')
+    except ValueError as Error:
+        logging.warning(f'{Error} ')
         return flask.Response(
-            'Ошибка при добавлении энергии',
+            'Ошибка при добавлении продукта',
             status=400
         )
     if form_id is None:
         return flask.Response(
-            'Ошибка при добавлении энергии',
+            'Ошибка при добавлении продукта',
             status=400
         )
-    count = int(flask.request.form.get('count', None))
-    name = flask.request.form.get('name', None).strip()
-    bank_account = flask.request.form.get('bank_account', None)
-    if None in (count, bank_account, name):
+    try:
+        data = {
+            'количество': int(flask.request.form.get('count', None)),
+            'сумма': int(flask.request.form.get('amount', None)),
+            'название товара': flask.request.form.get('name', None).strip(),
+            'cчет': int(flask.request.form.get('bank_account', None))
+        }
+    except Exception:
         return flask.Response(
-            'Ошибка получения данных',
+            'Ошибка при преобразовании данных',
             status=400
         )
-    product_id = env.db.impl().session.execute(
+
+    # execute data from database
+    product = env.db.impl().session.execute(
         sqlalchemy.select(
-            models.Product.id
+            models.Product
         )
-        .filter_by(name=name)
+        .filter_by(name=data['название товара'])
     ).scalars().first()
-    if product_id is None:
+    if product is None:
+        logging.warning(f'при действии мастера с товаром произошла ошибка: продукт не найден')
         return flask.Response(
-            'Продукт или ресурс не найден'
+            'Продукт не найден',
+            400
         )
+
+    # checking for the existence of bank_account
     check_bank_account = env.db.impl().session.execute(
         sqlalchemy.select(
             models.BankAccount
         )
-        .filter_by(id=bank_account)
+        .filter_by(id=data['счет'])
     ).first()
-    if not check_bank_account:
+    if check_bank_account is None:
+        logging.warning(f'при действии мастера с товаром произошла ошибка: счет не найден')
         return flask.Response(
-            'такого счета не существует',
+            'введенный счет не был найден',
             400
         )
     products2bank_account = env.db.impl().session.execute(
@@ -152,31 +89,109 @@ def action_with_resource():
             models.Product2BankAccount
         )
         .filter(sqlalchemy.and_(
-            models.Product2BankAccount.product_id == product_id,
-            models.Product2BankAccount.bank_account_id == bank_account
+            models.Product2BankAccount.product_id == product.id,
+            models.Product2BankAccount.bank_account_id == data['счет']
         ))
     ).scalars().first()
     if products2bank_account is None:
         products2bank_account = models.Product2BankAccount(
-            bank_account_id=int(bank_account),
-            product_id=product_id,
+            bank_account_id=int(data['счет']),
+            product_id=product.id,
             count=0
         )
         env.db.impl().session.add(products2bank_account)
         env.db.impl().session.commit()
-    if form_id in (3, 5):
-        products2bank_account.count = products2bank_account.count - int(count)
-    elif form_id in (4, 6):
-        products2bank_account.count += count
+    customer_wallet = env.db.impl().session.execute(
+        sqlalchemy.select(
+            models.Product2BankAccount
+        )
+        .filter_by(bank_account_id=data['счет'], product_id=1)
+    ).scalars().first()
+    if form_id == 3:
+        products2bank_account.count = products2bank_account.count - data['количество']
+        customer_wallet.count += data['сумма']
+    elif form_id == 4:
+        products2bank_account.count += data['количество']
+        customer_wallet.count = customer_wallet + data['сумма']
     try:
+        transaction = models.Transaction(
+            product.id,
+            int(data['счет']),
+            current_user.bank_account_id,
+            int(data['количество']),
+            amount=data['сумма'],
+            status='approved',
+            created_at=datetime.datetime.now(tz=CurrentTimezone),
+            updated_at=datetime.datetime.now(tz=CurrentTimezone),
+            comment=f'мастер перевел продукт {product.name} на счет {data["счет"]} в количестве {data["количество"]}'
+        )
+        logging.info(f'мастер перевел продукт {product.name} на счет {data["счет"]} в количестве {data["количество"]}')
+        env.db.impl().session.add(transaction)
         env.db.impl().session.commit()
     except Exception as Error:
+        env.db.impl().session.rollback()
         flask.Response(
             f'Произошла ошибка во время выполнения действия',
             400
         )
         logging.warning(f'{Error}')
-    return render_template('main/add_withdrowal.html')
+    return render_template('main/add_withdrowal.html', products=models.Product.get_all())
+
+
+@blueprints.master_blueprint.route('/master_add_money')
+@login_required
+def add_money():
+    try:
+        data = {
+            'форма': int(flask.request.form.get('form_id', None)),
+            'счет': int(flask.request.form.get('bank_account', None)),
+            'количество': int(flask.request.form.get('count', None))
+        }
+    except Exception as Error:
+        logging.warning('ошибка при получении данных')
+        return flask.Response(
+            'Ошибка при добавлении надиков',
+            400
+        )
+    user_wallet = env.db.impl().session.execute(
+        sqlalchemy.select(
+            models.Product2BankAccount
+        )
+        .filter_by(bank_account_id=data['счет'], product_id=1)
+    ).scalars().first()
+    if user_wallet is None:
+        logging.warning(f'При добавлении мастером {current_user.bank_account_id} надиков не был найден кошелек пользователя')
+        return flask.Response(
+            'Не был найден счет',
+            400
+        )
+
+    if data['форма'] == 1:
+        user_wallet.count -= data['количество']
+    elif data['фирма'] == 2:
+        user_wallet.count += data['количество']
+    try:
+        transaction = models.Transaction(
+            product_id=1,
+            customer_bank_account_id=current_user.bank_account_id,
+            seller_bank_account_id=data['счет'],
+            count=data['количество'],
+            amount=data['количество'],
+            status='approved',
+            created_at=datetime.datetime.now(tz=CurrentTimezone),
+            updated_at=datetime.datetime.now(tz=CurrentTimezone),
+            comment=f'мастер {current_user.bank_account} перевел продукт надики на счет {data["счет"]} в количестве {data["количество"]}'
+        )
+        logging.info(f'мастер {current_user.bank_account} перевел продукт надики на счет {data["счет"]} в количестве {data["количество"]}')
+        env.db.impl().session.add(transaction)
+        env.db.impl().session.commit()
+    except Exception as Error:
+        env.db.impl().session.rollback()
+        logging.warning(f'При пропытке добавить надики мастером {current_user.bank_account} на счет {data["счет"]} в количестве {data["количество"]}')
+        return flask.Response(
+            'при попытке добавить надики произошла ошибка'
+        )
+    return render_template('main/add_withdrowal.html', products=models.Product.get_all())
 
 
 @blueprints.master_blueprint.route('/create_office', methods=['POST'])
@@ -184,7 +199,7 @@ def action_with_resource():
 def create_office():
     office_data = {
         'company_account': int(flask.request.form.get('company_bank_account', None)),
-        'city_name': flask.request.form.get('city_bank_account', None).capitalize(),
+        'city_name': flask.request.form.get('city_bank_account', None),
         'amount': int(flask.request.form.get('amount', None))
     }
 
@@ -252,5 +267,6 @@ def create_office():
     )
     env.db.impl().session.add(office)
     env.db.impl().session.commit()
+    logging.info(f'матстер {current_user.bank_account_id} добавил офис в городе {city.name} для компании {company.name}')
 
     return render_template('main/create_office.html')
