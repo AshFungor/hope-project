5473636eb8572f1eb9f8944222282927f6cb2f60
miv1kk <96937481+miv1kk@users.noreply.github.com>
2024-06-30 00:12:14 +0300

    [fullstack] city-hall step-2 (#88)
    Co-authored-by: miv1kk <miv1kk1972@gmail.com>

--- app/routes/city_hall_lk.py
+++ app/routes/city_hall_lk.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, url_for, session
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 # local
 import app.modules.database.static as static
@@ -15,6 +15,9 @@ from app.routes.person_account import get_money
 @blueprints.accounts_blueprint.route('/city_hall_lk')
 @login_required
 def city_hall_cabinet():
+
+    loginned_user_id = current_user.id
+
     info = sqlalchemy.select(
             models.CityHall
     )
@@ -34,11 +37,16 @@ def city_hall_cabinet():
     economic_assistant = spec[economic_assistant_id]
     social_assistant = spec[social_assistant_id]
 
+    role = False
+    if loginned_user_id in (mayor_id, economic_assistant_id, social_assistant_id):
+        role = True
+
     return render_template('main/city_hall.html', 
                             session=session,
                             balance=get_money(bank_account),
                             bank_account=bank_account,
                             mayor=mayor,
                             economic_assistant=economic_assistant,
-                            social_assistant=social_assistant
+                            social_assistant=social_assistant,
+                            role=role
                             )
