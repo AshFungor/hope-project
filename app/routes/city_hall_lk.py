f1a1ba32293c25d761a302f7d91eab7f1f924e46
miv1kk <miv1kk1972@gmail.com>
2024-06-29 21:01:44 +0300

    cityhall

--- app/routes/city_hall_lk.py
+++ app/routes/city_hall_lk.py
@@ -5,9 +5,40 @@ from flask_login import login_required
 import app.modules.database.static as static
 import app.routes.blueprints as blueprints
 
+from app.env import env
+import app.models as models
+
+import sqlalchemy
+
+from app.routes.person_account import get_money
 
 @blueprints.accounts_blueprint.route('/city_hall_lk')
 @login_required
 def city_hall_cabinet():
-    """Личный кабинет мэрии"""
-    return render_template('main/city_hall.html', session=session)
+    info = sqlalchemy.select(
+            models.CityHall
+    )
+    data = env.db.impl().session.execute(info).scalars().first()
+    bank_account = data.bank_account_id
+    mayor_id = data.mayor_id
+    economic_assistant_id = data.economic_assistant_id
+    social_assistant_id = data.social_assistant_id
+
+    names = sqlalchemy.select(
+        models.User
+    ).filter(models.User.id.in_((mayor_id, economic_assistant_id, social_assistant_id)))
+    data = env.db.impl().session.execute(names).scalars().all()
+    spec = {name.id: name.full_name_string for name in data}
+
+    mayor = spec[mayor_id]
+    economic_assistant = spec[economic_assistant_id]
+    social_assistant = spec[social_assistant_id]
+
+    return render_template('main/city_hall.html', 
+                            session=session,
+                            balance=get_money(bank_account),
+                            bank_account=bank_account,
+                            mayor=mayor,
+                            economic_assistant=economic_assistant,
+                            social_assistant=social_assistant
+                            )
