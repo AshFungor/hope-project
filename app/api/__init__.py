e4835ef147ad1962b4196db813b28660eea16312
AshFungor <evgenynarkhov2@gmail.com>
2025-07-05 15:45:10 +0300

    init: fix ordering

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,2 +1,5 @@
-from . import city_hall, company, goal, login, prefecture, products, transactions
+# isort: off
 from .blueprints import Blueprints
+from . import city_hall, company, goal, login, prefecture, products, transactions
+
+# isort: on
